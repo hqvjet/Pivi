076d7cba3252081012d383e9b604f5bac991bba8
babynghe2003 <himinhpho@gmail.com>
2022-11-09 14:58:04 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Login from "./components/Signin";
 import Routes from "./routes";
 
+<<<<<<< HEAD
 const App = () => {
 
     return (
@@ -9,6 +10,18 @@ const App = () => {
         </>
     );
 
+=======
+function App() {
+    const [token, setToken]= useState(localStorage.getItem('user'))
+    // {token !== null ? <Routers/> : <Auth/>} 
+
+
+  return (
+      <>
+          <Routers/>
+      </>
+  );
+>>>>>>> 0aaf0e1 (add role server)
 }
 
 export default App;
